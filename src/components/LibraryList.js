833f1a5f42f816b2dff4c52f805427374da91ab9
github username <raustin.melchior@gmail.com>
2019-05-12 14:26:11 -0700

    add listItem component

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
-  renderItem(library) {}
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
 
   render() {
     return (
